2bdcf174bd8fbc3ecb9541050117bc25cb766bdd
Sven Fischer <sven@leiderfischer.de>
2024-01-22 13:00:32 +0100

    Disable beely nfs

--- hosts/common/optional/nfs.nix
+++ hosts/common/optional/nfs.nix
@@ -4,9 +4,9 @@
   pkgs,
   ...
 }: {
-  fileSystems."/media/beely" = {
-    device = "beely:/media/hdd";
-    fsType = "nfs4";
-    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" "softreval"];
-  };
+  # fileSystems."/media/beely" = {
+  #   device = "beely:/media/hdd";
+  #   fsType = "nfs4";
+  #   options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" "softreval"];
+  # };
 }
