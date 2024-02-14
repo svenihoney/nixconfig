c1bd9fee3020778c6667a3a5278757f5e529e2a4
Sven Fischer <fischer@software.ads>
2024-02-14 13:26:24 +0100

    Add filter settings for dodo

--- hosts/dodo/proxy.nix
+++ hosts/dodo/proxy.nix
@@ -19,10 +19,21 @@
     owner = "tinyproxy";
     mode = "0600";
   };
+  sops.secrets.filter_settings = {
+    # format = "yaml";
+    # can be also set per secret
+    sopsFile = ./secrets.yaml;
+    owner = "tinyproxy";
+    mode = "0600";
+  };
   services.tinyproxy = {enable = true;};
 
   systemd.services.tinyproxy.serviceConfig.ExecStart = let
     configFile = config.sops.secrets.proxy_settings.path;
   in
     pkgs.lib.mkForce "${pkgs.tinyproxy}/bin/tinyproxy -d -c ${configFile}";
+
+  networking.firewall = {
+    allowedTCPPorts = [8888];
+  };
 }
