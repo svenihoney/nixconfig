71ebf59bfdb3e9ff618f25b3ef786e37c4213862
Sven Fischer <sven@leiderfischer.de>
2024-03-21 23:18:09 +0100

    Use bash as default shell

--- hosts/common/users/fischer/default.nix
+++ hosts/common/users/fischer/default.nix
@@ -9,7 +9,7 @@ in {
   # users.mutableUsers = false;
   users.users.fischer = {
     isNormalUser = true;
-    shell = pkgs.fish;
+    # shell = pkgs.fish;
     extraGroups =
       [
         "wheel"
